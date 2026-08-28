3fd7e457e78c06d4f24f43ab1142da3610b000a7
Ishtiaq Kasem <kasem_i@yahoo.com>
2026-08-28 16:31:44 -0700

    add ParquetReader and related files

---
+++ ParquetSQLLoader.Core/Models/ParquetTableDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Text;
+
+namespace ParquetSQLLoader.Core.Models
+{
+    public class ParquetTableDefinition
+    {
+        public required string TableName { get; init; }
+        public required IReadOnlyList<ParquetColumnDefinition> Columns { get; init; }
+    }
+}
