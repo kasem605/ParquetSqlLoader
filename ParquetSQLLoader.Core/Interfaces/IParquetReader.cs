65584f8c272e6c07112c06c245b1ae4f86204f46
Ishtiaq Kasem <kasem_i@yahoo.com>
2026-08-28 16:31:44 -0700

    add ParquetReader and related files

---
+++ ParquetSQLLoader.Core/Interfaces/IParquetReader.cs
@@ -0,0 +1,12 @@
+﻿using ParquetSQLLoader.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParquetSQLLoader.Core.Interfaces
+{
+    public interface IParquetReader
+    {
+        Task<ParquetTableDefinition> ReadTableDefinitionAsync(string filePath);
+    }
+}
