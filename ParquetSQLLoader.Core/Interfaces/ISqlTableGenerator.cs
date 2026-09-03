e13a712a832adacb663a6963a7a3ab2ef8f8a28a
kasem <kasem_i@yahoo.com>
2026-09-03 15:34:36 -0700

    Add SQLTableGenerator

--- ParquetSQLLoader.Core/Interfaces/ISqlTableGenerator.cs
+++ ParquetSQLLoader.Core/Interfaces/ISqlTableGenerator.cs
@@ -4,6 +4,6 @@ namespace ParquetSQLLoader.Core.Interfaces
 {
     public interface ISqlTableGenerator
     {
-        Task CreateTableAsync(string schema, string tableName, ParquetSchemaDefinition definition, CancellationToken cancellationToken = default);
+        Task CreateTableAsync(ParquetTableDefinition schema, CancellationToken cancellationToken = default);
     }
 }
